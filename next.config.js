4c424aec958a02d7c15053bf80045eea59cd8435
Marcelo Anselmo de Souza Filho <marcelofilho@mpf.mp.br>
2021-10-26 12:22:25 -0300

    1 v

---
+++ next.config.js
@@ -0,0 +1,16 @@
+const withTM = require("next-transpile-modules")([
+  "@mui/material",
+  "@mui/system",
+]); // pass the modules you would like to see transpiled
+
+module.exports = withTM({
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@mui/styled-engine": "@mui/styled-engine-sc",
+    };
+    return config;
+  },
+  excludeFile: (str) => /\*.{spec,test}.js/.test(str),
+});
