53816ce57a9066a25e2590e66eedbd01e245ed69
Marcelo Anselmo de Souza Filho <marcelofilho@mpf.mp.br>
2021-10-27 19:14:51 -0300

    publicado pwa

--- next.config.js
+++ next.config.js
@@ -3,14 +3,21 @@ const withTM = require("next-transpile-modules")([
   "@mui/system",
 ]); // pass the modules you would like to see transpiled
 
-module.exports = withTM({
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@mui/styled-engine": "@mui/styled-engine-sc",
-    };
-    return config;
-  },
-  excludeFile: (str) => /\*.{spec,test}.js/.test(str),
-});
+const withPWA = require("next-pwa");
+
+module.exports = withPWA(
+  withTM({
+    pwa: {
+      dest: "public",
+    },
+    reactStrictMode: true,
+    webpack: (config) => {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "@mui/styled-engine": "@mui/styled-engine-sc",
+      };
+      return config;
+    },
+    excludeFile: (str) => /\*.{spec,test}.js/.test(str),
+  })
+);
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,3 @@
-import theme from "@styles/theme";
 import Document, { Head, Html, Main, NextScript } from "next/document";
 import React from "react";
 import { ServerStyleSheet } from "styled-components";
@@ -9,8 +8,31 @@ export default class MyDocument extends Document {
     return (
       <Html lang="pt-br">
         <Head>
-          {/* PWA primary color */}
-          <meta content={theme.palette.primary.main} name="theme-color" />
+          <link
+            rel="apple-touch-icon"
+            sizes="144x144"
+            href="/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/favicon/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
           <link
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
             rel="stylesheet"
